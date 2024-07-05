36165602a6cfb4bf25c24267a99b00182d0c9c7c
david carrillo <davycarmelo72ayy@gmail.com>
2024-07-04 23:47:52 -0500

    initial commit

---
+++ gamelet.js
@@ -0,0 +1,27 @@
+const ball = document.getElementById('ball');
+document.addEventListener('keydown', handleKeyPress);
+let position = 0;
+
+function handleKeyPress(e) {
+    if (e.code === "ArrowUp") {
+        position = position - 20;
+    }
+    if (e.code === 'ArrowDown') {
+        position = position + 20;
+    }
+    
+    if (e.code === 'ArrowLeft') {
+        position = position - 20;
+    }
+    if (e.code === 'ArrowRight') {
+        position = position + 20;
+    }
+    if (position < 0) {
+        position = 0;
+    }
+    refresh();
+}
+function refresh() {
+    ball.style.left = position + 'px'
+    
+}
